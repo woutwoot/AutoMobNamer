5a52996ff70de3a5790e435fd471ad3173866226
Warby <ultibyte@gmail.com>
2013-04-21 12:12:47 +0100

    Completed "/namer list" command

--- src/net/ultibyte/AutoMobNamer/CommandHandler.java
+++ src/net/ultibyte/AutoMobNamer/CommandHandler.java
@@ -58,205 +58,205 @@ public class CommandHandler implements CommandExecutor {
 				}
 				if (args[0].equalsIgnoreCase("list")){
 					if (args[1].equalsIgnoreCase("Bat")) {
-						commandsender.sendMessage(ChatColor.GREEN + "******************** Bat Names ********************");
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Bat Names ********************");
 						for (String SomeName : plugin.BatNames) {
-							commandsender.sendMessage(ChatColor.GREEN + SomeName);
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
 						}
 						return true;
 					}
 					if (args[1].equalsIgnoreCase("Chicken")) {
-						commandsender.sendMessage(ChatColor.GREEN + "******************** Chicken Names ********************");
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Chicken Names ********************");
 						for (String SomeName : plugin.ChickenNames) {
-							commandsender.sendMessage(ChatColor.GREEN + SomeName);
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
 						}
 						return true;
 					}
 					if (args[1].equalsIgnoreCase("Cow")) {
-						plugin.CowNames.clear();
-						plugin.CowNames.add("Cow");
-						commandsender.sendMessage(ChatColor.GREEN + "Cow names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Cow Names ********************");
+						for (String SomeName : plugin.CowNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("IronGolem")) || (args[1].equalsIgnoreCase("Iron_Golem")) || (args[1].equalsIgnoreCase("Villager_Golem")) || (args[1].equalsIgnoreCase("VillagerGolem"))) {
-						plugin.IronGolemNames.clear();
-						plugin.IronGolemNames.add("Iron Golem");
-						commandsender.sendMessage(ChatColor.GREEN + "Iron Golem names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Iron Golem Names ********************");
+						for (String SomeName : plugin.IronGolemNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Mooshroom")) || (args[1].equalsIgnoreCase("MushroomCow")) || (args[1].equalsIgnoreCase("Mushroom_Cow"))) {
-						plugin.MooshroomNames.clear();
-						plugin.MooshroomNames.add("Mooshroom");
-						commandsender.sendMessage(ChatColor.GREEN + "Mooshroom names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Mooshroom Names ********************");
+						for (String SomeName : plugin.MooshroomNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Ocelot")) || (args[1].equalsIgnoreCase("Cat"))) {
-						plugin.OcelotNames.clear();
-						plugin.OcelotNames.add("Ocelot");
-						commandsender.sendMessage(ChatColor.GREEN + "Mooshroom names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Ocelot Names ********************");
+						for (String SomeName : plugin.OcelotNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Pig"))) {
-						plugin.PigNames.clear();
-						plugin.PigNames.add("Pig");
-						commandsender.sendMessage(ChatColor.GREEN + "Pig names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Pig Names ********************");
+						for (String SomeName : plugin.PigNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Sheep"))) {
-						plugin.SheepNames.clear();
-						plugin.SheepNames.add("Sheep");
-						commandsender.sendMessage(ChatColor.GREEN + "Sheep names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Sheep Names ********************");
+						for (String SomeName : plugin.SheepNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Squid"))) {
-						plugin.SquidNames.clear();
-						plugin.SquidNames.add("Squid");
-						commandsender.sendMessage(ChatColor.GREEN + "Squid names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Squid Names ********************");
+						for (String SomeName : plugin.SquidNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Wolf")) || (args[1].equalsIgnoreCase("Dog"))) {
-						plugin.WolfNames.clear();
-						plugin.WolfNames.add("Wolf");
-						commandsender.sendMessage(ChatColor.GREEN + "Wolf names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Wolf Names ********************");
+						for (String SomeName : plugin.WolfNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Blaze"))) {
-						plugin.BlazeNames.clear();
-						plugin.BlazeNames.add("Blaze");
-						commandsender.sendMessage(ChatColor.GREEN + "Blaze names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Blaze Names ********************");
+						for (String SomeName : plugin.BlazeNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("CaveSpider")) || (args[1].equalsIgnoreCase("Cave_Spider"))) {
-						plugin.CaveSpiderNames.clear();
-						plugin.CaveSpiderNames.add("Cave Spider");
-						commandsender.sendMessage(ChatColor.GREEN + "Cave Spider names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Cave Spider Names ********************");
+						for (String SomeName : plugin.CaveSpiderNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("Creeper"))) {
-						plugin.CreeperNames.clear();
-						plugin.CreeperNames.add("Creeper");
-						commandsender.sendMessage(ChatColor.GREEN + "Creeper names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Creeper Names ********************");
+						for (String SomeName : plugin.CreeperNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 					if ((args[1].equalsIgnoreCase("EnderDragon")) || (args[1].equalsIgnoreCase("Ender_Dragon")) || (args[1].equalsIgnoreCase("Dragon"))) {
-						plugin.EnderDragonNames.clear();
-						plugin.EnderDragonNames.add("EnderDragon");
-						commandsender.sendMessage(ChatColor.GREEN + "EnderDragon names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** EnderDragon Names ********************");
+						for (String SomeName : plugin.EnderDragonNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Enderman")) {
-						plugin.EndermanNames.clear();
-						plugin.EndermanNames.add("Enderman");
-						commandsender.sendMessage(ChatColor.GREEN + "Enderman names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Enderman Names ********************");
+						for (String SomeName : plugin.EndermanNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Ghast")) {
-						plugin.GhastNames.clear();
-						plugin.GhastNames.add("Ghast");
-						commandsender.sendMessage(ChatColor.GREEN + "Ghast names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Ghast Names ********************");
+						for (String SomeName : plugin.GhastNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("MagmaCube") || args[1].equalsIgnoreCase("LavaCube") || args[1].equalsIgnoreCase("LavaSlime") || args[1].equalsIgnoreCase("MagmaSlime")) {
-						plugin.MagmaCubeNames.clear();
-						plugin.MagmaCubeNames.add("MagmaCube");
-						commandsender.sendMessage(ChatColor.GREEN + "MagmaCube names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Magma Cube Names ********************");
+						for (String SomeName : plugin.MagmaCubeNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Silverfish")) {
-						plugin.SilverfishNames.clear();
-						plugin.SilverfishNames.add("Silverfish");
-						commandsender.sendMessage(ChatColor.GREEN + "Silverfish names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Silverfish Names ********************");
+						for (String SomeName : plugin.SilverfishNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Skeleton")) {
-						plugin.SkeletonNames.clear();
-						plugin.SkeletonNames.add("Skeleton");
-						commandsender.sendMessage(ChatColor.GREEN + "Skeleton names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Skeleton Names ********************");
+						for (String SomeName : plugin.SkeletonNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Slime")) {
-						plugin.SlimeNames.clear();
-						plugin.SlimeNames.add("Slime");
-						commandsender.sendMessage(ChatColor.GREEN + "Slime names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Slime Names ********************");
+						for (String SomeName : plugin.SlimeNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("SnowGolem") || args[1].equalsIgnoreCase("SnowMan")) {
-						plugin.SnowGolemNames.clear();
-						plugin.SnowGolemNames.add("Slime");
-						commandsender.sendMessage(ChatColor.GREEN + "SnowGolem names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Snow Golem Names ********************");
+						for (String SomeName : plugin.SnowGolemNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Spider")) {
-						plugin.SpiderNames.clear();
-						plugin.SpiderNames.add("Spider");
-						commandsender.sendMessage(ChatColor.GREEN + "Spider names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Spider Names ********************");
+						for (String SomeName : plugin.SpiderNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Witch")) {
-						plugin.WitchNames.clear();
-						plugin.WitchNames.add("Witch");
-						commandsender.sendMessage(ChatColor.GREEN + "Witch names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Witch Names ********************");
+						for (String SomeName : plugin.WitchNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Wither")) {
-						plugin.WitherNames.clear();
-						plugin.WitherNames.add("Wither");
-						commandsender.sendMessage(ChatColor.GREEN + "Wither names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Wither Names ********************");
+						for (String SomeName : plugin.WitherNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Zombie")) {
-						plugin.ZombieNames.clear();
-						plugin.ZombieNames.add("Zombie");
-						commandsender.sendMessage(ChatColor.GREEN + "Zombie names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Zombie Names ********************");
+						for (String SomeName : plugin.ZombieNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("ZombiePigman") || args[1].equalsIgnoreCase("Pigman")) {
-						plugin.ZombiePigmanNames.clear();
-						plugin.ZombiePigmanNames.add("ZombiePigman");
-						commandsender.sendMessage(ChatColor.GREEN + "ZombiePigman names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Zombie Pigman Names ********************");
+						for (String SomeName : plugin.ZombiePigmanNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 
 					if (args[1].equalsIgnoreCase("Villager")) {
-						plugin.VillagerNames.clear();
-						plugin.VillagerNames.add("Villager");
-						commandsender.sendMessage(ChatColor.GREEN + "Villager names have been cleared!");
-						plugin.saveConfig();
+						commandsender.sendMessage(ChatColor.BLUE + "******************** Villager Names ********************");
+						for (String SomeName : plugin.VillagerNames) {
+							commandsender.sendMessage(ChatColor.AQUA + SomeName);
+						}
 						return true;
 					}
 				}
